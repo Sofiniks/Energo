1780a13b6d3ca0ab1f56e874f582b964d8ce1c22
amckey <67048650+amckey@users.noreply.github.com>
2022-11-03 09:53:13 +0200

    Update ContactUs.js

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -146,7 +146,7 @@ const Form = () => {
                                 <ul className={styles.contacts_socials}>
                                     <li className={styles.contacts_socials_item}><a href={contacts.phone}><img src={whatsapp} alt="socials" /></a></li>
                                     <li className={styles.contacts_socials_item}><a href={contacts.fb}><img src={facebook} alt="socials" /></a></li>
-                                    <li className={styles.contacts_socials_item}><a href="/"><img src={instagram} alt="socials" /></a></li>
+                                    <li className={styles.contacts_socials_item}><a href={contacts.in}><img src={instagram} alt="socials" /></a></li>
                                 </ul>
 
                             </div>
